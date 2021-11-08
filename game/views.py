627bc6d7f05862b12c6f05ea26f0ebec6be6a94b
hongwei niu <1433394534@qq.com>
2021-11-09 01:43:15 +0800

    first page

--- game/views.py
+++ game/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse("<h1>哈哈哈123</h1>")
+
+def play(request):
+    return HttpResponse("<h1>游戏界面</h1>")
+
